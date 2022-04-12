40e3091af3c153c3ca70e2612e8c269ef08415a9
Sumedin Bilalic <sumko87@gmail.com>
2022-04-12 22:12:56 +0200

    fixed mili

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -3,9 +3,9 @@ const milili = [10, 25, 4];
 // Using mili = [4, 10, 25] would be invalid
 function myArr(mili) {
     'use strict';
-    milili[0]=4;
-    milili[1]=10;
-    milili[2]=25;
+    mili[0]=4;
+    mili[1]=10;
+    mili[2]=25;
     return mili;
 }
 console.log(myArr(milili));
